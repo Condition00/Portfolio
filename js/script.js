f60f5af33008364d9287daea6acb368d61fb2906
Anant Kavuru <anant.kavuru@gmail.com>
2024-07-12 22:36:29 +0530

    commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var typed= new Typed("#text",{
+    strings: ["Developer","Artist","Digital Craftsman"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+ScrollReveal().reveal('.contain','.contain2');
