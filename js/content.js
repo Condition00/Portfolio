97d2b1246b0ecc55cda8201c6993888e0f83b455
Anant Kavuru <anant.kavuru@gmail.com>
2024-07-15 23:22:26 +0530

    skills page in progress

--- js/content.js
+++ js/content.js
@@ -4,16 +4,3 @@ $('.skill-per').each(function(){
     $this.css("width",per+'%');
 });
 
-let number=document.getElementById("number");
-let counter=0;
-setInterval(()=>{
-    if(counter==65){
-        clearInterval();
-    }else{
-        counter+=1;
-    number.innerHTML=counter + "%";
-
-    }
-    
-},30)
-// 2000/percentage
