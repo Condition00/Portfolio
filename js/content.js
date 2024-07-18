9a33072a45e2c026749bec36d4f5eb29150bf6f9
Anant Kavuru <anant.kavuru@gmail.com>
2024-07-18 21:52:16 +0530

    main portfolio finished

--- js/content.js
+++ js/content.js
@@ -4,3 +4,22 @@ $('.skill-per').each(function(){
     $this.css("width",per+'%');
 });
 
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
