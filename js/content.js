0b7283441faab063dd3a0edd32d9c6b70633b642
Anant Kavuru <anant.kavuru@gmail.com>
2024-07-19 14:18:41 +0530

    js update

--- js/content.js
+++ js/content.js
@@ -23,3 +23,21 @@ inputs.forEach((input) => {
   input.addEventListener("blur", blurFunc);
 });
 
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a [href*=' + id + ')').classList.add ('active');
+      });
+    };
+  });
+};
