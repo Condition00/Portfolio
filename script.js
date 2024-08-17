9aef63eb446ade3eb57a9ab121956269b5da1609
Anant Kavuru <anant.kavuru@gmail.com>
2024-08-17 22:27:01 +0530

    major 1 page content allocate

--- script.js
+++ script.js
@@ -5,3 +5,48 @@ var typed= new Typed("#text",{
     backDelay:1000,
     loop:true
 });
+
+$('.skill-per').each(function(){
+    var $this = $(this);
+    var per = $this.attr('per');
+    $this.css("width",per+'%');
+});
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a [href*=' + id + ')').classList.add ('active');
+      });
+    };
+  });
+};
+
